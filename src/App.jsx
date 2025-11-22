c99ad15d6f308055e37047c6a79cccd2a2fe391d
Kudhan <kudhanshaik03@gmail.com>
2025-11-22 21:32:41 +0530

    added app changes

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
             path="/dashboard"
             element={
               <Protected>
-                <Dashboard />
+                <Dashbord />
               </Protected>
             }
           />
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -239,7 +239,7 @@ export default function Dashboard() {
               }`}
             >
               Next
-            </buttn>
+            </button>
           </div>
         </div>
       )}
