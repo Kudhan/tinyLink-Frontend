7c334340399ee7bed8da2f585e5d72fe864672d9
Kudhan <kudhanshaik03@gmail.com>
2025-11-22 21:33:07 +0530

    toast change

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
             path="/dashboard"
             element={
               <Protected>
-                <Dashbord />
+                <Dashboard />
               </Protected>
             }
           />
